52a3ef38445bfd84257186e6056675de9aeb134e
erykszczesniak <eryks453@gmail.com>
2020-10-06 10:33:41 +0200

    toolbar

--- Web Browse/Web Browse/WebViewController.swift
+++ Web Browse/Web Browse/WebViewController.swift
@@ -8,6 +8,29 @@ class WebViewController: UIViewController {
         return view as? WKWebView
     }
     
+    
+    lazy var backButton = UIBarButtonItem(barButtonSystemItem: .rewind, target: webView, action: #selector(WKWebView.goBack))
+    
+    lazy var forwardButton = UIBarButtonItem(barButtonSystemItem: .fastForward, target: webView, action: #selector(WKWebView.goForward))
+    
+    lazy var searchController = UISearchController(searchResultsController: nil)
+    
+    var url: URL? {
+        get {
+            return webView?.url
+        }
+        set {
+            if let url = newValue {
+                load(url: url)
+            }
+        }
+    }
+    
+    func load(url: URL) {
+        let request = URLRequest(url: url)
+        webView?.load(request)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,6 +39,14 @@ class WebViewController: UIViewController {
             webView?.load(request)
         }
         
+        toolbarItems = createToolbarItems()
+        navigationItem.searchController = searchController
+        
+    }
+    
+    
+    func createToolbarItems() -> [UIBarButtonItem] {
+        return [backButton, forwardButton]
     }
 
 
